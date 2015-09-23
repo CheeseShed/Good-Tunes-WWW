43e187bcd539a1b8a3032cc59256c76b753f165b
Ian Huntington <ihuntington85@gmail.com>
2015-09-23 19:39:07 +0100

    Updated package and config files

--- config.js
+++ config.js
@@ -16,14 +16,19 @@ System.config({
 
 System.config({
   "map": {
-    "angular": "github:angular/bower-angular@1.4.4",
+    "angular": "github:angular/bower-angular@1.4.6",
     "angular-resource": "github:angular/bower-angular-resource@1.4.4",
+    "angular-sanitize": "github:angular/bower-angular-sanitize@1.4.6",
     "angular-ui-router": "github:angular-ui/ui-router@0.2.15",
     "babel": "npm:babel-core@5.8.22",
     "babel-runtime": "npm:babel-runtime@5.8.20",
     "core-js": "npm:core-js@0.9.18",
+    "lodash": "npm:lodash@3.10.1",
     "github:angular-ui/ui-router@0.2.15": {
-      "angular": "github:angular/bower-angular@1.4.4"
+      "angular": "github:angular/bower-angular@1.4.6"
+    },
+    "github:angular/bower-angular-sanitize@1.4.6": {
+      "angular": "github:angular/bower-angular@1.4.6"
     },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
@@ -35,6 +40,9 @@ System.config({
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:lodash@3.10.1": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
     }
   }
 });
