cdd6593898c197df4ed51192c0e52ace6e81de11
Ian Huntington <ihuntington85@gmail.com>
2016-03-18 13:14:50 +0000

    Added ESLint with the ESLint Standard configuration and Angular config files

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,22 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const eslint = require('gulp-eslint');
+
+gulp.task('lint', function () {
+    return gulp.src('src/**/*.js')
+        .pipe(eslint())
+        .pipe(eslint.format());
+});
 
 gulp.task('sass', function () {
-  return gulp
+    return gulp
     .src('src/css/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('default', ['sass'], function () {
-  gulp.watch('src/css/**/*.scss', ['sass']);
+    gulp.watch('src/css/**/*.scss', ['sass']);
 });
